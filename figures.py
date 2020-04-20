4a743784ab4b84092594a03f2000bb9105b6fdce
Anton Brisilin <a.brisilin@innopolis.university>
2020-04-20 20:30:36 +0300

    refactor figures

--- figures.py
+++ figures.py
@@ -20,24 +20,26 @@ class FigureType(Enum):
     # Cross = 4
 
 
-def random_figure():
-    """Returns random  suprematism figure"""
-    figures_dict = {
-        FigureType.Circle: Circle(),
-        FigureType.Rectangle: Rectangle()}
-    _type = rand.choice(list(FigureType))
-    return figures_dict[_type]
-
-
 class Figure:
     """Base class for figures. Should never be instantiated"""
-    max_size = [256, 256]
+    MAX_SIZE = [256, 256]
     figure_type = None
+
+    class FigureData:
+        """General figure data"""
+        color: [int, int, int]
+        center: [int, int]
+
+        def __init__(self, color: [int, int, int] = None):
+            if color is None:
+                color = unit.Unit.TARGET[self.center[0], self.center[1], :]
+            self.color = color
 
     def __init__(self):
         self.data = None
 
     def inside(self, point: [int, int]) -> bool:
+        """Check given point to be inside of current figure"""
         vertices = self.data.vertices()
 
         polygon_area = geo.pivotal_area(vertices)
@@ -45,52 +47,55 @@ class Figure:
 
         return abs(polygon_area - area_from_external_point) < 1e-5
 
-    def translate(self, translation_vector):
+    def translate(self, translation_vector: [int, int]) -> None:
+        """Moves figure by translation vector, by changing its center coordinates"""
         self.data.center += np.asarray(translation_vector)
-        rad = self.data.radius
-        lower_bound = [rad+1, rad+1]
-        upper_bound = [(i-1) - rad for i in constants.IMAGE_SIZE]
+        radius = self.data.radius
+        lower_bound = [radius+1, radius+1]
+        upper_bound = [(i-1) - radius for i in constants.IMAGE_SIZE]
         self.data.center = np.clip(
             self.data.center, a_min=lower_bound, a_max=upper_bound)
 
-    def scale(self, scale):
+    def scale(self, scale: float) -> None:
+        """Scales figure by scale times. Clips it to max size, if new size is too large"""
+        center = self.data.center
+        tmp = [512 - item for item in center]
+        max_rad = min(center + tmp + [i/2 for i in Figure.MAX_SIZE])
         self.data.radius *= scale
+        self.data.radius = np.clip(self.data.radius, a_min=0, a_max=max_rad)
 
 
 class Circle(Figure):
+    """Circle class"""
     figure_type = FigureType.Circle
 
-    # Necessary data to create circle
-    class CircleData:
+    class CircleData(Figure.FigureData):
+        """Necessary data to create circle"""
         radius: int
         center: [int, int]
-        color: [int, int, int]
 
-        def __init__(self, r: int, c: [int, int]):
-            self.radius = r
-            self.center = c
-            self.color = unit.Unit.TARGET[c[0], c[1], :]
+        def __init__(self, radius: int, center: [int, int]):
+            self.radius = radius
+            self.center = center
+            super(Circle.CircleData, self).__init__()
 
-    def __init__(self, is_random=True, data: CircleData = None):
-        if is_random:
-            center, radius = self.random_circle()
-            self.data = Circle.CircleData(radius, center)
-        elif data:
-            self.data = copy.deepcopy(data)
-        else:
-            raise Exception('You should either provide both '
-                            'random as false, and data or neither of them')
+    def __init__(self, data: CircleData):
+        super().__init__()
+        self.data = copy.deepcopy(data)
+        if data is None:
+            raise Exception('Data is none')
 
-    def inside(self, point: [int, int]):
+    def inside(self, point: [int, int]) -> bool:
+        """check the point to be inside the figure"""
         return geo.distance(self.data.center, point) < self.data.radius
 
-    def draw(self):
+    def draw(self) -> np.ndarray:
+        """Returns coordinates of circle that can be used for indexing image to
+        fill part of it with color of figure"""
         return draw.circle(self.data.center[1], self.data.center[0], self.data.radius)
 
-    def change_color(self, color: [int, int, int]):
-        raise NotImplementedError('')
-
-    def intersects(self, other: Figure):
+    def intersects(self, other: Figure) -> bool:
+        """check 2 figures for intersection"""
         if other.figure_type == FigureType.Circle:
             center = self.data.center
             other_center = other.data.center
@@ -98,48 +103,43 @@ class Circle(Figure):
             return dist < (self.data.radius + other.data.radius)
         if other.figure_type == FigureType.Rectangle:
             return other.intersects(self)
+        return False
 
-    def covers(self, other):
+    def covers(self, other: Figure) -> bool:
+        """check if current figure covers other and makes it invisible"""
         if other.figure_type == FigureType.Circle:
             center = self.data.center
             other_center = other.data.center
             dist = geo.distance(center, other_center)
-            R = self.data.radius
-            r = other.data.radius
-            return dist + r <= R
+            radius_1 = self.data.radius
+            radius_2 = other.data.radius
+            return dist + radius_2 <= radius_1
         if other.figure_type == FigureType.Rectangle:
             vertices = other.data.vertices()
             for i in range(0, len(vertices[0])):
                 if not self.inside(vertices[i]):
                     return False
             return True
+        return False
 
-    @staticmethod
-    def random_circle(min_rad=30):
-        center = [randint(1, constants.IMAGE_SIZE[i] - 1) for i in range(0, 2)]
-        tmp = [512 - item for item in center]
-        max_rad = min(center + tmp + [i/2 for i in Figure.max_size])
-        if max_rad < min_rad:
-            center, radius = Circle.random_circle()
-        else:
-            radius = randint(min_rad, max_rad)
-        return [center, radius]
-
-    def rotate(self, degrees):
-        pass
+    def rotate(self, _) -> None:
+        """does nothing, rotation do not make sense for circles"""
+        return
 
 
 class Rectangle(Figure):
+    """Rectangle class"""
     figure_type = FigureType.Rectangle
 
-    # Necessary data to create rectangle:
-    # circumscribed circle and two angles
     class RectangleData(Circle.CircleData):
+        """Necessary data to create rectangle: circumscribed circle and two angles"""
+
         def __init__(self, r: int, c: [int, int], thetas: (float, float)):
             super().__init__(r, c)
             self.angles = thetas
 
-        def vertices(self):
+        def vertices(self) -> np.array:
+            """returns vertices of the rectangle"""
             vertices_x = []
             vertices_y = []
             center = self.center
@@ -159,33 +159,24 @@ class Rectangle(Figure):
 
             return np.asarray(ret)
 
-    def __init__(self, is_random=True, data: RectangleData = None):
-        if is_random:
-            center, radius = Circle.random_circle(min_rad=50)
-            angle1 = randint(0, 360)
-            angle2 = randint(0, 360)
-            if abs(angle1 % 180 - angle2 % 180) < 30:
-                angle2 = angle1 + 30
-            self.data = Rectangle.RectangleData(
-                radius, center, (angle1, angle2))
-        elif data:
-            self.data = copy.deepcopy(data)
-        else:
+    def __init__(self, data: RectangleData):
+        super().__init__()
+        self.data = copy.deepcopy(data)
+        if data is None:
             raise Exception('You should either provide both'
                             'random as false, and data or neither of them')
 
     def draw(self):
+        """Returns coordinates of rectangle that can be used for indexing image to
+        fill part of it with color of figure"""
         vertices_x = [i[0] for i in self.data.vertices()]
         vertices_y = [i[1] for i in self.data.vertices()]
         return draw.polygon(vertices_y, vertices_x)
 
-    def change_color(self, color: (int, int, int)):
-        raise NotImplementedError('')
-
     def intersects(self, other: Figure):
+        """check 2 figures for intersection"""
         if other.figure_type == FigureType.Circle:
-            new_circle = Circle()
-            new_circle.data = self.data
+            new_circle = Circle(data=self.data)
             if not new_circle.intersects(other):
                 return False
             vertices = self.data.vertices()
@@ -215,6 +206,7 @@ class Rectangle(Figure):
         return False
 
     def covers(self, other: Figure):
+        """check if current figure covers other and makes it invisible"""
         if other.figure_type == FigureType.Circle:
             if self.inside(other.data.center):
                 vertices = self.data.vertices()
@@ -232,6 +224,41 @@ class Rectangle(Figure):
             return True
         return False
 
-    def rotate(self, degrees):
+    def rotate(self, degrees: int):
+        """rotates self around center on degrees degrees"""
         for i in self.data.angles:
             i += degrees
+
+
+def random_circle(min_rad: int = 30) -> Circle:
+    """creates and returns random circle"""
+    center = [randint(1, constants.IMAGE_SIZE[i] - 1) for i in range(0, 2)]
+    tmp = [512 - item for item in center]
+    max_rad = min(center + tmp + [i/2 for i in Figure.MAX_SIZE])
+    if max_rad < min_rad:
+        return random_circle()
+
+    radius = randint(min_rad, max_rad)
+    data = Circle.CircleData(radius, center)
+    return Circle(data)
+
+
+def random_rectangle() -> Rectangle:
+    """creates and returns random rectangle"""
+    circle = random_circle(min_rad=50)
+    center = circle.data.center
+    radius = circle.data.radius
+    angle1 = randint(0, 360)
+    angle2 = randint(0, 360)
+    if abs(angle1 % 180 - angle2 % 180) < 30:
+        angle2 = angle1 + 30
+    return Rectangle(Rectangle.RectangleData(radius, center, (angle1, angle2)))
+
+
+def random_figure() -> Figure:
+    """Returns random  suprematism figure"""
+    figures_dict = {
+        FigureType.Circle: random_circle(),
+        FigureType.Rectangle: random_rectangle()}
+    _type = rand.choice(list(FigureType))
+    return figures_dict[_type]
